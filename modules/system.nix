08f8fbef8a2174f323ac534b9f4ddc4ef1ae259a
Prn-Ice <stormprince77@gmail.com>
2023-08-21 08:59:55 +0100

    fix: set wayland session as default

--- modules/system.nix
+++ modules/system.nix
@@ -25,6 +25,7 @@
   # Enable the KDE Plasma Desktop Environment.
   services.xserver.displayManager.sddm.enable = true;
   services.xserver.desktopManager.plasma5.enable = true;
+  services.xserver.displayManager.defaultSession = "plasmawayland";
 
   # Configure keymap in X11
   services.xserver = {
