a4a6f15f5169b44024ecded993a7f2e7809d982e
Prn-Ice <stormprince77@gmail.com>
2023-08-26 22:27:54 +0100

    fix: dont edit hardware config

--- hosts/nixos/default.nix
+++ hosts/nixos/default.nix
@@ -26,6 +26,9 @@
     efi.canTouchEfiVariables = true;
   };
 
+  # Use latest kernel
+  boot.kernelPackages = pkgs.linuxPackages_latest;
+
   networking.hostName = "nixos"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
--- hosts/nixos/hardware-configuration.nix
+++ hosts/nixos/hardware-configuration.nix
@@ -9,7 +9,6 @@
       (modulesPath + "/installer/scan/not-detected.nix")
     ];
 
-  boot.kernelPackages = pkgs.linuxPackages_latest;
   boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "usbhid" "usb_storage" "sd_mod" "sdhci_pci" ];
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ "kvm-amd" ];
