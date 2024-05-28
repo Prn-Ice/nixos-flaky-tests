e72a4671cba87d3ee181b907ba95964bdd5e2b08
Prn-Ice <stormprince77@gmail.com>
2024-05-28 07:51:40 +0100

    chore: undo audio kernel patch

--- hosts/nixos/hardware/default.nix
+++ hosts/nixos/hardware/default.nix
@@ -1,5 +1,11 @@
-{ ... }:
+{ config, pkgs, ... }:
 
+# let
+#   lenovo-speaker-fix = pkgs.callPackage ./audio/lenovo-16ARHA7_speaker-fix.nix {
+#     # Make sure the module targets the same kernel as your system is using.
+#     inherit (config.boot.kernelPackages) kernel;
+#   };
+# in
 {
   imports = [
     ./nvidia.nix
@@ -12,4 +18,6 @@
 
   # √(3200² + 2000²) px / 16 in ≃ 236 dpi
   # services.xserver.dpi = 236;
+
+  # boot.extraModulePackages = [ lenovo-speaker-fix ];
 }
