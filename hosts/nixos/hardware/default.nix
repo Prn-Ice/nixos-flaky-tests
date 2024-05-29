bcd6cffe09a601ba58b92ec366f7c9f4e102c01f
Prn-Ice <stormprince77@gmail.com>
2024-05-29 23:37:26 +0100

    chore: add openRGB with i2c error fix

--- hosts/nixos/hardware/default.nix
+++ hosts/nixos/hardware/default.nix
@@ -1,4 +1,4 @@
-{ ... }:
+{ pkgs, ... }:
 
 {
   imports = [
@@ -12,4 +12,25 @@
 
   # √(3200² + 2000²) px / 16 in ≃ 236 dpi
   # services.xserver.dpi = 236;
+
+  # rgb
+  services.udev.packages = [ pkgs.openrgb-with-all-plugins ];
+  boot.kernelModules = [ "eeprom" "ee1004" "i2c-core" "i2c-dev" "i2c-piix4" ];
+  hardware.i2c.enable = true;
+
+  environment.systemPackages = with pkgs; [
+    i2c-tools
+  ];
+
+  users.extraUsers.prnice = {
+    extraGroups = [ "i2c" ];
+  };
+
+  services = {
+    hardware.openrgb = {
+      enable = true;
+      motherboard = "amd";
+      package = pkgs.openrgb-with-all-plugins;
+    };
+  };
 }
