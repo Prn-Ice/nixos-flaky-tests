e5df9c9cbbf81a4792f3c65e03173244bd16e2dc
Prn-Ice <stormprince77@gmail.com>
2024-05-29 22:41:07 +0100

    chore: ideapad_laptop specialisation

--- hosts/nixos/hardware/default.nix
+++ hosts/nixos/hardware/default.nix
@@ -1,11 +1,5 @@
-{ config, pkgs, ... }:
+{ ... }:
 
-# let
-#   lenovo-speaker-fix = pkgs.callPackage ./audio/lenovo-16ARHA7_speaker-fix.nix {
-#     # Make sure the module targets the same kernel as your system is using.
-#     inherit (config.boot.kernelPackages) kernel;
-#   };
-# in
 {
   imports = [
     ./nvidia.nix
@@ -18,6 +12,4 @@
 
   # √(3200² + 2000²) px / 16 in ≃ 236 dpi
   # services.xserver.dpi = 236;
-
-  # boot.extraModulePackages = [ lenovo-speaker-fix ];
 }
--- hosts/nixos/hardware/legion_slim.nix
+++ hosts/nixos/hardware/legion_slim.nix
@@ -1,10 +1,17 @@
-{ config, pkgs, ... }:
+{ config, lib, pkgs, ... }:
 
 {
   # Tools for managing legion laptop hardware
 
-  # fix sleep issue
+  # Fix sleep issue
   boot.blacklistedKernelModules = [ "ideapad_laptop" ];
+
+  # Ignore sleep fix. For testing purposes only
+  specialisation = {
+    with-ideapad-laptop.configuration = {
+      boot.blacklistedKernelModules = lib.mkForce (lib.filter (module: module != "ideapad_laptop") config.boot.blacklistedKernelModules);
+    };
+  };
 
   boot.extraModulePackages = with config.boot.kernelPackages; [ lenovo-legion-module ];
 
