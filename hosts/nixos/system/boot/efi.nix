50db537b85890e9b9e112111547bdd798a6de13a
Prn-Ice <stormprince77@gmail.com>
2024-05-31 09:04:15 +0100

    chore: update grub config

--- hosts/nixos/system/boot/efi.nix
+++ hosts/nixos/system/boot/efi.nix
@@ -1,5 +1,11 @@
 { pkgs, ... }:
 
+let
+  grub-theme = pkgs.sleek-grub-theme.override {
+    withStyle = "dark";
+    withBanner = "Choose Your Destiny"; 
+  };
+in
 {
   # Bootloader
   boot.loader = {
@@ -8,9 +14,17 @@
     };
     grub = {
       efiSupport = true;
+      useOSProber = true;
       device = "nodev";
+      memtest86.enable = true;
+      theme = grub-theme;
     };
   };
+
+  # Make sure our package is installed
+  environment.systemPackages = [
+    grub-theme
+  ];
 
   # Use latest kernel
   boot.kernelPackages = pkgs.linuxPackages_latest;
