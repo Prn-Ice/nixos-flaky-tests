b3ea069f017d6cbf6ecda334666e4cd93752b7a6
Prn-Ice <stormprince77@gmail.com>
2024-10-03 18:47:27 +0100

    fix: lenovo-legion postPatch error fix

--- hosts/nixos/hardware/legion_slim.nix
+++ hosts/nixos/hardware/legion_slim.nix
@@ -3,6 +3,30 @@
 {
   # Tools for managing legion laptop hardware
 
+  # Default package fails to build
+  # Override package to use my branch and remove broken postPatch step
+  nixpkgs.overlays = [
+    (final: prev: {
+      lenovo-legion = prev.lenovo-legion.overrideAttrs (old: {
+        src = prev.fetchFromGitHub {
+          owner = "Prn-Ice";
+          repo = "LenovoLegionLinux";
+          rev = "18c88217c13829df78ad135b6582207fcb2757ea";
+          hash = "sha256-mGYm5SPnji4SntZiOE2RxuaV0Qex+Wuhrs+kME4hfyQ=";
+        };
+
+        postPatch = ''
+          substituteInPlace ./setup.cfg \
+            --replace-fail "_VERSION" "0.0.12"
+          substituteInPlace ./legion_linux/legion.py \
+            --replace-fail "/etc/legion_linux" "$out/share/legion_linux"
+          substituteInPlace ./legion_linux/legion_gui.desktop \
+            --replace-fail "Icon=/usr/share/pixmaps/legion_logo.png" "Icon=legion_logo"
+        '';
+      });
+    })
+  ];
+
   # Fix sleep issue
   boot.blacklistedKernelModules = [ "ideapad_laptop" ];
 
