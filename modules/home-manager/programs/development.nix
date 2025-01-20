fd67205aa812a8bd7d36c0eebd09a93493e81386
Prn-Ice <stormprince77@gmail.com>
2025-01-20 16:47:54 +0100

    feat: package android-studio with sdk

--- modules/home-manager/programs/development.nix
+++ modules/home-manager/programs/development.nix
@@ -1,7 +1,17 @@
-{pkgs, ...}: {
+{pkgs, ...}: let
+  buildToolsVersion = "34.0.0";
+  androidComposition = pkgs.androidenv.composeAndroidPackages {
+    buildToolsVersions = [buildToolsVersion "28.0.3"];
+    platformVersions = ["34" "28"];
+    abiVersions = ["armeabi-v7a" "arm64-v8a"];
+    includeNDK = true;
+  };
+  androidSdk = androidComposition.androidsdk;
+in {
   home.packages = with pkgs; [
     # android
-    android-studio
+    (android-studio.withSdk androidSdk)
+
     # jetbrains.idea-community
 
     # networking tools
