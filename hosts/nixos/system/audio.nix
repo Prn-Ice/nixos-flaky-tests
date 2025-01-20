586cc6f9494d85583255a803243554058dbad770
Prn-Ice <stormprince77@gmail.com>
2025-01-20 16:43:29 +0100

    fix: follow api updates

--- hosts/nixos/system/audio.nix
+++ hosts/nixos/system/audio.nix
@@ -2,8 +2,8 @@
 
 {
   # Enable sound with pipewire.
-  hardware.pulseaudio.enable = false;
   security.rtkit.enable = true;
+  services.pulseaudio.enable = false;
   services.pipewire = {
     enable = true;
     pulse.enable = true;
