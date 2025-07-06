7e4c57aa478d221cbb0f51f1e0ad590e50037c40
Prn-Ice <stormprince77@gmail.com>
2025-07-06 17:13:31 +0100

    feat: update windsurf

--- modules/home-manager/programs/pkgs/windsurf/default.nix
+++ modules/home-manager/programs/pkgs/windsurf/default.nix
@@ -1,47 +1,43 @@
-{ lib
-, stdenv
-, callPackage
-, fetchurl
-, commandLineArgs ? ""
-, useVSCodeRipgrep ? stdenv.hostPlatform.isDarwin
-, writeScript
-, bash
-}:
-
-let
-  plat = "linux-x64";  # Assuming it's only for x86_64 Linux, adjust if needed
-  archive_fmt = "tar.gz";
-  sha256 = "sha256-7lpKw4+aJRilRCnLI1uudtdLP/8PWUfb/ClzjXjyhUI=";
-
+{
+  lib,
+  stdenv,
+  callPackage,
+  fetchurl,
+  commandLineArgs ? "",
+  useVSCodeRipgrep ? stdenv.hostPlatform.isDarwin,
+  writeScript,
+  bash,
+}: let
+  sha256 = "sha256-jgSSS5PiPtnKAOtvQyEiVS9osjYLoZPjl4xmJuKXKag=";
 in
-callPackage ./generic.nix rec {
-  inherit commandLineArgs useVSCodeRipgrep;
+  callPackage ./generic.nix {
+    inherit commandLineArgs useVSCodeRipgrep;
 
-  version = "1.9.2";
-  pname = "windsurf";
+    version = "1.10.7";
+    pname = "windsurf";
 
-  executableName = "windsurf";
-  longName = "Windsurf";
-  shortName = "windsurf";
+    executableName = "windsurf";
+    longName = "Windsurf";
+    shortName = "windsurf";
 
-  src = fetchurl {
-    url = "https://windsurf-stable.codeiumdata.com/${plat}/stable/8cb7f313303c8b35844a56b6fe0f76e508261569/Windsurf-${plat}-${version}.${archive_fmt}";
-    inherit sha256;
-  };
+    src = fetchurl {
+      url = "https://windsurf-stable.codeiumdata.com/linux-x64/stable/7c493d782a6cad0516e79f070d953687991df4ec/Windsurf-linux-x64-1.10.7.tar.gz";
+      inherit sha256;
+    };
 
-  sourceRoot = "";
+    sourceRoot = "";
 
-  updateScript = writeScript "update-windsurf.sh" ''
-    #!${bash}/bin/bash
-    exec ${bash}/bin/bash ${./update-windsurf.sh}
-  '';
+    updateScript = writeScript "update-windsurf.sh" ''
+      #!${bash}/bin/bash
+      exec ${bash}/bin/bash ${./update-windsurf.sh}
+    '';
 
-  meta = with lib; {
-    description = "Windsurf application";
-    homepage = "https://codeium.com/windsurf/";
-    license = licenses.unfree;
-    maintainers = with maintainers; [ Prn-Ice ];
-    platforms = [ "x86_64-linux" ];
-    sourceProvenance = with sourceTypes; [ binaryNativeCode ];
-  };
-}
+    meta = with lib; {
+      description = "Windsurf application";
+      homepage = "https://codeium.com/windsurf/";
+      license = licenses.unfree;
+      maintainers = with maintainers; [Prn-Ice];
+      platforms = ["x86_64-linux"];
+      sourceProvenance = with sourceTypes; [binaryNativeCode];
+    };
+  }
