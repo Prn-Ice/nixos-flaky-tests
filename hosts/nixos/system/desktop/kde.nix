0b19d5ade3102072ec8643a18412a2936c0d9821
Prn-Ice <stormprince77@gmail.com>
2025-07-21 10:52:46 +0100

    feat: more kde packages

--- hosts/nixos/system/desktop/kde.nix
+++ hosts/nixos/system/desktop/kde.nix
@@ -15,6 +15,7 @@
 
   services.displayManager = {
     sddm.wayland.enable = true;
+    sddm.settings.General.DisplayServer = "wayland";
     sddm.enable = true;
     sddm.enableHidpi = true;
     defaultSession = "plasma";
@@ -29,14 +30,26 @@
 
   environment.systemPackages = with pkgs; [
     kdePackages.yakuake
-    kdePackages.sddm-kcm
     kdePackages.krecorder
     kdePackages.kget
     kdePackages.dragon
     kdePackages.kdenlive
     kdePackages.spectacle
     kdePackages.filelight
-    haruna
+
+    kdePackages.kcalc # Calculator
+    kdePackages.kcharselect # Tool to select and copy special characters from all installed fonts
+    kdePackages.kcolorchooser # A small utility to select a color
+    kdePackages.kolourpaint # Easy-to-use paint program
+    kdePackages.ksystemlog # KDE SystemLog Application
+    kdePackages.sddm-kcm # Configuration module for SDDM
+    kdiff3 # Compares and merges 2 or 3 files or directories
+    kdePackages.isoimagewriter # Optional: Program to write hybrid ISO files onto USB disks
+    kdePackages.partitionmanager # Optional Manage the disk devices, partitions and file systems on your computer
+    hardinfo2 # System information and benchmarks for Linux systems
+    haruna # Open source video player built with Qt/QML and libmpv
+    wayland-utils # Wayland utilities
+    wl-clipboard # Command-line copy/paste utilities for Wayland
 
     # Theme packages
     (pkgs.where-is-my-sddm-theme.override {
