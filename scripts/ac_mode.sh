e132944256fbef249b679bf343e4f3b5cb631afb
Prn-Ice <stormprince77@gmail.com>
2025-07-31 12:24:52 +0100

    fix: refresh rate script updates

--- scripts/ac_mode.sh
+++ scripts/ac_mode.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
-kscreen-doctor output.eDP-1.mode.3200x2000@165
+panel_name=$(kscreen-doctor --outputs | grep 'Panel' -B 4 | grep 'Output:' | awk '{print $3}')
+kscreen-doctor "output.${panel_name}.mode.3200x2000@165"
