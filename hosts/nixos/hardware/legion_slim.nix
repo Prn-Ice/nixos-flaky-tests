75e393ff78ad92c8fac63e87bb71629767f4c72b
Prn-Ice <stormprince77@gmail.com>
2025-07-31 12:21:48 +0100

    fix: some experiments, disable speaker amp driver

--- hosts/nixos/hardware/legion_slim.nix
+++ hosts/nixos/hardware/legion_slim.nix
@@ -1,26 +1,14 @@
 {
+  lib,
   config,
   pkgs,
   ...
-}: {
+}:
+{
+  # NOTE: Experiments start
+
   # With the kernel module loaded, power-profiles change in response to FN+Q
   # Needs a restart if you make changes to the module
-
-  # Deep sleep experiment
-  # boot.kernelParams = [
-  #   "mem_sleep_default=deep"
-  # ];
-
-  # Resume experiment
-  # powerManagement = {
-  #   enable = true;
-  #   powerDownCommands = ''
-  #     modprobe -r goodix
-  #   '';
-  #   resumeCommands = ''
-  #     modprobe goodix
-  #   '';
-  # };
 
   # Systemd service test
   #systemd.services.yoga-bass-speaker-fix = {
@@ -37,10 +25,6 @@
   #  };
   #};
 
-  # Blacklisting this stops the annoying i2c error messages
-  # Also stops sound from working
-  # boot.blacklistedKernelModules = ["snd_hda_scodec_cs35l41_i2c"];
-
   # Other options experiment
   # modinfo snd_hda_scodec_cs35l41
   # options snd_hda_scodec_cs35l41 firmware_autostart=0
@@ -48,51 +32,55 @@
   # Sources:
   # https://gist.github.com/felipelalli/6179aac72735fd35ea3a9854beb490e5
   # https://github.com/NixOS/nixos-hardware/blob/master/system76/darp6/default.nix
-  hardware.firmware = [
-    (pkgs.writeTextFile {
-      name = "legion-alc287-patch";
-      destination = "/lib/firmware/legion-alc287-patch";
-      text = ''
-        [codec]
-        0x10ec0287 0x17aa38b6 0
+  # hardware.firmware = [
+  #   (pkgs.writeTextFile {
+  #     name = "legion-alc287-patch";
+  #     destination = "/lib/firmware/legion-alc287-patch";
+  #     text = ''
+  #       [codec]
+  #       0x10ec0287 0x17aa38b6 0
 
-        [model]
-        auto
+  #       [model]
+  #       auto
 
-        [verb]
-        0x20 0x500 0x24
-        0x20 0x400 0x41
-        0x20 0x500 0x26
-        0x20 0x400 0x2
-        0x20 0x400 0x0
-        0x20 0x400 0x0
-        0x20 0x4b0 0x20
-        0x20 0x500 0x24
-        0x20 0x400 0x42
-        0x20 0x500 0x26
-        0x20 0x400 0xc
-        0x20 0x400 0x0
-        0x20 0x400 0x2a
-        0x20 0x4b0 0x20
-        0x20 0x500 0x26
-        0x20 0x400 0x2
-        0x20 0x400 0x0
-        0x20 0x400 0x0
-        0x20 0x4b0 0x20
+  #       [verb]
+  #       0x20 0x500 0x24
+  #       0x20 0x400 0x41
+  #       0x20 0x500 0x26
+  #       0x20 0x400 0x2
+  #       0x20 0x400 0x0
+  #       0x20 0x400 0x0
+  #       0x20 0x4b0 0x20
+  #       0x20 0x500 0x24
+  #       0x20 0x400 0x42
+  #       0x20 0x500 0x26
+  #       0x20 0x400 0xc
+  #       0x20 0x400 0x0
+  #       0x20 0x400 0x2a
+  #       0x20 0x4b0 0x20
+  #       0x20 0x500 0x26
+  #       0x20 0x400 0x2
+  #       0x20 0x400 0x0
+  #       0x20 0x400 0x0
+  #       0x20 0x4b0 0x20
 
-        [hint]
-        auto_mute = no
-      '';
-    })
-  ];
+  #       [hint]
+  #       auto_mute = no
+  #     '';
+  #   })
+  # ];
 
-  boot.extraModprobeConfig = ''
-    options snd_hda_intel model=auto power_save=0 beep_mode=0 patch=legion-alc287-patch
-  '';
+  # boot.extraModprobeConfig = ''
+  #   options snd_hda_intel model=auto patch=legion-alc287-patch
+  # '';
+
+  # NOTE: Experiments end
 
   # Override package to use my branch, test fan read fix
   nixpkgs.overlays = [
-    (final: prev: let
+    (
+      final: prev:
+      let
       # For local development
       # lenovo-legion-src = lib.fileset.toSource {
       #   root = /home/prnice/Projects/personal/LenovoLegionLinux;
@@ -104,7 +92,8 @@
         rev = "read_file_fix";
         hash = "sha256-Wp/Kha5Wa+XJnQYZt8uzaWwkf9uPllp2TIXIVz60eqQ=";
       };
-    in rec {
+      in
+      rec {
       lenovo-legion = prev.lenovo-legion.overrideAttrs (old: {
         src = lenovo-legion-src;
 
@@ -123,11 +112,23 @@
 
         sourceRoot = "${lenovo-legion.src.name}/kernel_module";
       });
-    })
+      }
+    )
   ];
 
   boot.extraModulePackages = with config.boot.kernelPackages; [
     lenovo-legion-module
+    zenpower
+  ];
+
+  # zenpower experiment
+  boot.kernelModules = [ "zenpower" ];
+
+  # Blacklisting this stops the annoying i2c error messages
+  # Also stops sound from working
+  boot.blacklistedKernelModules = [
+    "k10temp"
+    "snd_hda_scodec_cs35l41_i2c"
   ];
 
   environment.systemPackages = with pkgs; [
