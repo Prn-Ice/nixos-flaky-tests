9d897d5c01be7bad90dcbb8e0a7b47c3ded6ca38
Prn-Ice <stormprince77@gmail.com>
2025-12-17 14:00:56 +0100

    feat: update default android composition

--- modules/home-manager/programs/development.nix
+++ modules/home-manager/programs/development.nix
@@ -1,13 +1,31 @@
-{pkgs, ...}: let
+{ pkgs, ... }:
+let
   androidComposition = pkgs.androidenv.composeAndroidPackages {
-    buildToolsVersions = ["latest" "35.0.0" "34.0.0" "28.0.3" "27.0.1"];
-    platformVersions = ["latest" "34" "28" "27"];
-    abiVersions = ["armeabi-v7a" "arm64-v8a"];
-    ndkVersions = ["28.1.13356709" "27.0.12077973"];
+    buildToolsVersions = [
+      "latest"
+      "35.0.0"
+      "34.0.0"
+      "28.0.3"
+      "27.0.1"
+    ];
+    platformVersions = [
+      "34"
+      "28"
+      "27"
+    ];
+    abiVersions = [
+      "armeabi-v7a"
+      "arm64-v8a"
+    ];
+    ndkVersions = [
+      "28.1.13356709"
+      "27.0.12077973"
+    ];
     includeNDK = true;
   };
   androidSdk = androidComposition.androidsdk;
-in {
+in
+{
   home.packages = with pkgs; [
     # android
     (android-studio.withSdk androidSdk)
